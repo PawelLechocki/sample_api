f4d591380f2a5c48e3e600604e68d9407ffa6661
PawelLechocki <pawel.lechocki@uk.ey.com>
2023-07-06 12:19:04 +0100

    sample store contract names

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const app = express();
 const port = 1234;
 
 const sampleDB = {
-  "0xC89Ce4735882C9F0f0FE26686c53074E09B0D550": {
+  "Tesco": {
     address: "0xC89Ce4735882C9F0f0FE26686c53074E09B0D550",
     network: "ethereum",
     deploymentBlock: 5,
   },
-  "0x9b1f7F645351AF3631a656421eD2e40f2802E6c0": {
+  "Waitrose": {
     address: "0x9b1f7F645351AF3631a656421eD2e40f2802E6c0",
     network: "ethereum",
     deploymentBlock: 11,
   },
-  "0x0E696947A06550DEf604e82C26fd9E493e576337": {
+  "Morrisons": {
     address: "0x0E696947A06550DEf604e82C26fd9E493e576337",
-    network: "arbitrum",
+    network: "ethereum",
     deploymentBlock: 17,
   },
 };
