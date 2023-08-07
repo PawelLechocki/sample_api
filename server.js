ffa61810a87ccc6248661ba76fcd7c8240d02776
PawelLechocki <pawel.lechocki@uk.ey.com>
2023-08-07 13:53:43 +0100

    reverted changes

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || "80";
-const mongoose = require("mongoose");
-const dotenv = require('dotenv').config({path: '.env'});
+const port = process.env.PORT || '80';
 
-app.get("/contracts/:id", async (req, res) => {
-  const connection = process.env.CONNECTION
-  console.log(connection)
-  await mongoose.connect(connection);
+const sampleDB = {
+  "Tesco": {
+    address: "0xC89Ce4735882C9F0f0FE26686c53074E09B0D550",
+    network: "ethereum",
+    deploymentBlock: 5,
+  },
+  "Waitrose": {
+    address: "0x9b1f7F645351AF3631a656421eD2e40f2802E6c0",
+    network: "ethereum",
+    deploymentBlock: 11,
+  },
+  "Morrisons": {
+    address: "0x0E696947A06550DEf604e82C26fd9E493e576337",
+    network: "ethereum",
+    deploymentBlock: 17,
+  },
+};
+
+app.get("/contracts/:id", (req, res) => {
   const id = req.params.id;
   const contract = sampleDB[id];
-  res.send("success");
+  res.send(JSON.stringify(contract));
 });
 
 app.listen(port, () => {
