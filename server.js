ebc7d942428648add16b026a3fda05ad1c51c7b1
PawelLechocki <pawel.lechocki@uk.ey.com>
2023-08-07 13:46:10 +0100

    test conn string

--- server.js
+++ server.js
@@ -1,29 +1,16 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || '80';
+const port = process.env.PORT || "80";
+const mongoose = require("mongoose");
+const dotenv = require('dotenv').config({path: '.env'});
 
-const sampleDB = {
-  "Tesco": {
-    address: "0xC89Ce4735882C9F0f0FE26686c53074E09B0D550",
-    network: "ethereum",
-    deploymentBlock: 5,
-  },
-  "Waitrose": {
-    address: "0x9b1f7F645351AF3631a656421eD2e40f2802E6c0",
-    network: "ethereum",
-    deploymentBlock: 11,
-  },
-  "Morrisons": {
-    address: "0x0E696947A06550DEf604e82C26fd9E493e576337",
-    network: "ethereum",
-    deploymentBlock: 17,
-  },
-};
-
-app.get("/contracts/:id", (req, res) => {
+app.get("/contracts/:id", async (req, res) => {
+  const connection = process.env.CONNECTION
+  console.log(connection)
+  await mongoose.connect(connection);
   const id = req.params.id;
   const contract = sampleDB[id];
-  res.send(JSON.stringify(contract));
+  res.send("success");
 });
 
 app.listen(port, () => {
